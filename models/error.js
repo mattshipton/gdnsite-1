29e9dca32d7ae597b97bc6ac39f111fd3b6676fe
Anthony Michelizzi <cynical89@gmail.com>
2017-03-26 12:08:44 -0700

    unique id for each error

--- models/error.js
+++ models/error.js
@@ -2,11 +2,14 @@
 const config = require("../config.json");
 
 const moment = require("moment-timezone");
+const Chance = require("chance");
+const chance = new Chance();
 
 module.exports = {
     newError: (message, user) => {
         const err = {
             error: false,
+            id: chance.guid(),
             message: message,
             user: user,
             dateTime: moment().tz(config.site.timezone).format("llll")
