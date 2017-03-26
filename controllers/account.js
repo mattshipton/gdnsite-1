1702185bea91ae12c45bd7c28c5990a2ac061298
Anthony Michelizzi <cynical89@gmail.com>
2017-03-25 23:52:25 -0700

    destroy is not a function, setting session to null

--- controllers/account.js
+++ controllers/account.js
@@ -15,7 +15,7 @@ module.exports.login = function* login() {
 
 module.exports.logout = function* logout() {
 	this.logout();
-	this.session.destroy();
+	this.session = null;
 	this.redirect("/");
 };
 
