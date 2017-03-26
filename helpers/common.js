9b401148ebf141215a52ecea184afa646ca7109d
Anthony Michelizzi <cynical89@gmail.com>
2017-03-26 15:33:53 -0700

    start of admin panel #12

--- helpers/common.js
+++ helpers/common.js
@@ -1,9 +1,20 @@
 "use strict";
 
+const adminPermission = 1341520961;
+
 module.exports = {
 	sortbyVotes: (themes) => {
 		return themes.sort((a,b) => {
 			return b.value.votes - a.value.votes;
 		});
+	},
+	getPermissions: (user) => {
+		for (const guild of user.guilds) {
+			if (guild.name === "Game Dev Network" && (guild.permissions === adminPermission
+				|| guild.owner === true)) {
+				user.admin = true;
+			}
+		}
+		return user;
 	}
 };
