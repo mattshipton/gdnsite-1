5ef1e9db08ff1f9129cb15e9e46eb87b3f9a1d66
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-25 16:37:12 -0400

    Fixed import path error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import ClientRouter from './components/ClientRouter';
+import ClientRouter from './components/ClientRouter/ClientRouter';
 
 class App extends Component {
   render() {
