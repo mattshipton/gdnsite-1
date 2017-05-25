751f813d5c6f7bc801393017fdf689e49f1122f3
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-25 00:10:26 -0400

    Created basic home page component to be updated later with content as is seen fit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className="text-center">This is Home</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
