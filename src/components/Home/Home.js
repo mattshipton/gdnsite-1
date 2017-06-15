b17a69330a8cd225ea89f5c161f891cc710e0a23
Eric Bastarache <eric_bastarache@hotmail.com>
2017-06-15 18:40:52 -0400

    Updated structure of home component to allow for new styling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,20 +9,25 @@ const Home = () => {
         <section className="j-header">
           <div className="col-md-12">
             <h1 className="text-center">Game Jam Essentials</h1>
-            <img src="" alt="Game Developers Network Logo" className="img-responsive"/>
-            <h4>Want to meet people, brainstorm, discuss, design, code, drink, sleep, experiment,
-              have fun and make great games? This is exactly the place for you!
-            </h4>
-            <p className="description">Did you know that we have gamejams monthly? You do now! GameJams are a great way
-              to work with artists of all kinds as well as other game developers to make an awesome game. Did
-              we also mention there's a sticker in it for you as well as a champ role on our Discord? Well
-              we did now!</p>
+            <img src="" alt="Game Developers Network Logo" className="img-responsive center-img"/>
+            <div className="row">
+              <div className="col-md-4 col-md-offset-4">
+                <h4>Want to meet people, brainstorm, discuss, design, code, drink, sleep, experiment,
+                  have fun and make great games? This is exactly the place for you!
+                </h4>
+                <p className="description">Did you know that we have gamejams monthly? You do now! GameJams are a great way
+                  to work with artists of all kinds as well as other game developers to make an awesome game. Did
+                  we also mention there's a sticker in it for you as well as a champ role on our Discord? Well
+                  we did now!</p>
+              </div>
+            </div>
             <Link className="join-btn" to="https://discord.gg/sWsrrJQ" target="_blank">Join GDN</Link>
           </div>
         </section>
       </div>
       <div className="row">
         <section className="q-boxes">
+          <hr />
           <div className="col-md-4">
             <h4>What will we do?</h4>
             <p>After the Game Jam themes are announced, the brainstorming begins, languages and frameworks are chosen, and the teams
@@ -40,7 +45,8 @@ const Home = () => {
           </div>
         </section>
       </div>
-      <div className="row">
+      <hr />
+      <div className="row push-down">
         <div className="col-md-12">
           <Link to="/jams" className="view-btn">View Jams</Link>
         </div>
